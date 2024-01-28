70fada6c89a49cb222d2320fdeaaa85ef5685f0c
Onur Ozgur OZKAN <onur.ozgur.ozkan@bulutfon.com>
2024-01-28 15:50:14 +0000

    Initial commit.

---
+++ lib/hipcall_whichtech.ex
@@ -0,0 +1,18 @@
+defmodule HipcallWhichtech do
+  @moduledoc """
+  Documentation for `HipcallWhichtech`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HipcallWhichtech.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hipcall_whichtech_test.exs
@@ -0,0 +1,8 @@
+defmodule HipcallWhichtechTest do
+  use ExUnit.Case
+  doctest HipcallWhichtech
+
+  test "greets the world" do
+    assert HipcallWhichtech.hello() == :world
+  end
+end
