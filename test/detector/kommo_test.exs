8768897333c51488d334794b3a9d0ca686bf651a
Meliha Çakmak <meliha.cakmak@bulutfon.com>
2026-01-27 10:17:34 +0300

    chore: Update dependencies

--- test/detector/kommo_test.exs
+++ test/detector/kommo_test.exs
@@ -8,10 +8,12 @@ defmodule HipcallWhichtech.Detector.KommoTest do
       html = ~s(<script src="https://gso.kommo.com/"></script>)
       assert Kommo.detect(html) == true
     end
+
     test "returns true when HTML contains drive-g.kommo.com pattern" do
       html = ~s(<script src="https://drive-g.kommo.com/"></script>)
       assert Kommo.detect(html) == true
     end
+
     test "returns true when HTML contains lc-en.kommo.com pattern" do
       html = ~s(<script src="wss://lc-en.kommo.com/"></script>)
       assert Kommo.detect(html) == true
