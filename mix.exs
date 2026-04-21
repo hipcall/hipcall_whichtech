f944cb5bd8c14874d78ce4274d51853017725705
Meliha Çakmak <meliha.cakmak@bulutfon.com>
2026-04-21 10:09:29 +0300

    chore: Bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule HipcallWhichtech.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/hipcall/hipcall_whichtech"
-  @version "0.13.2"
+  @version "0.13.3"
 
   def project do
     [
